45abd71035ba65828311560912f552171dda8c7e
Arif uz zaman <arifuzzaman959@gmail.com>
2024-01-03 10:28:22 +0600

    counter reducer,  store and counterSlice created

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <button>Increment</button>0<button>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
